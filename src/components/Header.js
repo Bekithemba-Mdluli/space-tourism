3f9f765f70e48fd00b33883d5ffbe9a12af639c7
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-04-21 15:28:27 +0200

    Added initial nav style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/shared/logo.svg';
+import menu from '../assets/shared/icon-hamburger.svg';
+import close from '../assets/shared/icon-close.svg';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleNav = () => {
+        setIsOpen(!isOpen)
+    }
     return (
-        <header className='header'>
-            <div>
-                <img src={logo} alt="Logo" />
-            </div>
-        </header>
+        <>
+            <header className='header'>
+                <div className='header__logo'>
+                    <img src={logo} alt="Logo" />
+                </div>
+
+                <div className='header__menu hide-for-tablet' onClick={toggleNav}>
+                    <img src={menu} alt="menu" />
+                </div>
+
+                {/* <div className='l'> */}
+                {/* <hr className='line hft' /> */}
+                <nav className='header__nav hide-for-mobile'>
+                    <ul className='header__nav-ul'>
+                        <li className='active'><a href='/'><span className='hft'>00</span>Home</a></li>
+                        <li><a href='/'><span className='hft'>01</span>Crew</a></li>
+                        <li><a href='/'><span className='hft'>02</span>Technology</a></li>
+                        <li><a href='/'><span className='hft'>03</span>Destination</a></li>
+                    </ul>
+                </nav>
+
+
+                {/* </div> */}
+            </header>
+            <nav className={isOpen ? 'header__navM' : 'header__navM hide'} data-visible="true">
+                <div className='header__navM-img'>
+                    <img src={close} alt="Close" onClick={toggleNav} />
+                </div>
+                <ul>
+                    <li className='active'><a href='/'><span>00</span>Home</a></li>
+                    <li><a href='/'><span>01</span>Destination</a></li>
+                    <li><a href='/'><span>02</span>Crew</a></li>
+                    <li><a href='/'><span>03</span>Technology</a></li>
+                </ul>
+            </nav>
+        </>
     )
 }
-
+// 00 Home
+// 01 Destination
+// 02 Crew
+// 03 Technology
 export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 function Home() {
     return (
-        <div>
+        <div className='home'>
             <Header />
         </div>
     )
