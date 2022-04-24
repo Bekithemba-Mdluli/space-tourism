35b67885d23cef7c428f6471a21c92afbfcb548c
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-04-24 10:10:06 +0200

    Added destination

---
+++ src/components/Destination-Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DestinationH() {
+    return (
+        <div className='dh'>
+            <ul className='dh__header'>
+                <li className='bActive'><button>Moon</button></li>
+                <li><button>Mars</button></li>
+                <li><button>Europa</button></li>
+                <li><button>Titan</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default DestinationH
