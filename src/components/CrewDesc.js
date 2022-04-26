8801b80d01acc102baac088c1f86605d31dd478b
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-04-26 14:57:10 +0200

    Added crew initial styling

--- src/components/CrewDesc.js
+++ src/components/CrewDesc.js
@@ -5,24 +5,33 @@ import doug from '../assets/crew/image-douglas-hurley.png';
 function CrewDesc() {
     return (
         <div className='crew-desc'>
-            <h4><span>02</span>Meet your crew</h4>
+            <h4 className='hide-for-desktop'><span>02</span>Meet your crew</h4>
 
-            <div className='crew-desc__img'>
-                <img src={doug} alt="Douglas"></img>
-            </div>
-            <hr />
-
-            <div>
-                <div className='crew-desc__radio'>
-                    <button className='crew-desc__radio-btn rActive'></button>
-                    <button className='crew-desc__radio-btn'></button>
-                    <button className='crew-desc__radio-btn'></button>
-                    <button className='crew-desc__radio-btn'></button>
+            <div className='crew-container'>
+                <div className='crew-desc__img'>
+                    <img src={doug} alt="Douglas"></img>
                 </div>
+                <hr className='hr-crew hide-for-tablet' />
 
-                <div className='crew-desc__content'>
-                    <h1><span>Commander</span> Douglas Hurley</h1>
-                    <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                <div>
+                    <div className='crew-desc__radio hide-for-tablet'>
+                        <button className='crew-desc__radio-btn rActive'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                    </div>
+
+                    <div className='crew-desc__content'>
+                        <h4 className='hft'><span>02</span>Meet your crew</h4>
+                        <h1><span>Commander</span><br /> Douglas Hurley</h1>
+                        <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                    </div>
+                    <div className='crew-desc__radio hide-for-mobile'>
+                        <button className='crew-desc__radio-btn rActive'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                        <button className='crew-desc__radio-btn'></button>
+                    </div>
                 </div>
             </div>
 
